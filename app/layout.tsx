0b0fc5680f2136aa8e9500ae9d7176b9a09ed944
daveprayag <prayagdave28@gmail.com>
2024-02-01 17:50:22 +0530

    Changed a few components v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
-import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GridSmallBackground } from "@/components/ui/grid-background";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,11 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} text-gray-50 text-opacity-90`}>
-        <div className="w-full bg-black dark:bg-dot-white/[0.15] bg-grid-small-black/[0.2]">
+        <div className="w-full bg-zinc-950 dark:bg-dot-white/[0.3] bg-grid-small-black/[0.2]">
           <ActiveSectionContextProvider>
             <Header />
             {children}
-            <Analytics />
+            <SpeedInsights />
             <Footer />
             <Toaster position="top-right" />
           </ActiveSectionContextProvider>
